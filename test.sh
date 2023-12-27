b9efdc6398d150a6e7fcb1fcdbc6a03930e0b652
devops-hemant <devopshemant@gmail.com>
2023-12-27 19:05:07 +0530

    test

--- test.sh
+++ test.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 filenames=$(cat files.txt)
 
-for i in $filenames
+for file in $filenames
 do
-    echo "file is $i"
+    if [[ -f $file ]]
+    then
+        case $file in
+            *.py)
+            echo "$file is a Python file"
+            ;;
+
+            *.html)
+            echo "$file is a html file"
+            ;;
+
+            *.sql)
+            echo "$file is a sql file"
+            ;;
+
+            *.r)
+            echo "$file is a .r file"
+            ;;
+
+            *)
+            echo "$file is not a compatible file format"
+            ;;
+        esac
+    else
+        echo "$file file not avaialble"
+    fi
 done
